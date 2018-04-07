5e3b9885aa27efe7187a87fd3b2034dd832dbf67
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-04-06 22:32:00 -0300

    TUALIZAÇÕES

--- public/frontend/js/app.js
+++ public/frontend/js/app.js
@@ -61224,7 +61224,7 @@ var staticRenderFns = [
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "container-fluid" }, [
+    return _c("div", { staticClass: "container-fluid no-padding" }, [
       _c("img", {
         staticClass: "img-fluid",
         attrs: { src: "/img/banner_principal_83.jpg" }
