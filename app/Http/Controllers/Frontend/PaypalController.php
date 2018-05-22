071fe975691677348be52f739396d487b55212ab
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-05-22 17:03:47 -0300

    atualizações

--- app/Http/Controllers/Frontend/PaypalController.php
+++ app/Http/Controllers/Frontend/PaypalController.php
@@ -90,7 +90,8 @@ class PaypalController extends Controller
 
         $balance = Balance::where('owner_id', '=', $order->owner_id)->get()->first();
 
-
+        Log::info($balance);
+        
         $historicBalance = new HistoricBalance;
         $historicBalance->paypal_order_id = $paypayOrder->id;
         $historicBalance->type = 1;
@@ -98,7 +99,7 @@ class PaypalController extends Controller
         $historicBalance->description = 'deposit';
         $historicBalance->balance_id = $balance->id;
         $historicBalance->from = $balance->value + $request->payment_gross;
-        $historicBalance->owner_id = $order->owner_id;
+        $historicBalance->owner_id = $balance->owner_id;
 
         $balance->value += $request->payment_gross;
 
