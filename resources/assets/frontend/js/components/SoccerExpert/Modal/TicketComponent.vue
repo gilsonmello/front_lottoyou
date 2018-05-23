d10bcb2fc02b23cb5f297f32a84264c392c56e45
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-05-23 13:29:18 -0300

    atualizações

--- resources/assets/frontend/js/components/SoccerExpert/Modal/TicketComponent.vue
+++ resources/assets/frontend/js/components/SoccerExpert/Modal/TicketComponent.vue
@@ -205,10 +205,10 @@
 
             //Abrindo o modal
             $('.modal-ticket').on('hidden.bs.modal', (event) => {
+            	console.log(this.empty)
         		if(this.ticket.complete == false && this.ticket.choseGoldBall && this.empty == true) {
         			toastr.error('Por favor, informe todos os jogos.', 'Cartela incompleta');
         		} else if(this.ticket.choseGoldBall == false && this.allSelected == false) {
-        			console.log(this.allSelected)
         			toastr.error('Por favor, informe todos os jogos e a Bola Lottoyou.', 'Cartela incompleta');
         		} else if(this.ticket.choseGoldBall == false) {
         			toastr.error('Por favor, selecione a Bola Lottoyou.', 'Cartela incompleta');
