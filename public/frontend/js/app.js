d593eae2ca48b031a847fb041c90f4d867889616
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-05-23 13:10:38 -0300

    atualizações

--- public/frontend/js/app.js
+++ public/frontend/js/app.js
@@ -99574,7 +99574,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 			if (_this4.ticket.complete == false && _this4.ticket.choseGoldBall && _this4.empty == true) {
 				toastr.error('Por favor, informe todos os jogos.', 'Cartela incompleta');
 			} else if (_this4.ticket.choseGoldBall == false && _this4.empty == false) {
-				toastr.error('Por favor, Está falando algum jogo e a Bola Lottoyou.', 'Cartela incompleta');
+				toastr.error('Por favor, selecione a Bola Lottoyou.', 'Cartela incompleta');
 			} else if (_this4.ticket.choseGoldBall == false) {
 				toastr.error('Por favor, selecione a Bola Lottoyou.', 'Cartela incompleta');
 			} else if (_this4.empty) {
