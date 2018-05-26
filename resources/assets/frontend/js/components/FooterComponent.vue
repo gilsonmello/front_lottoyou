0dacb63f5aa3a53ca2993d26116e38c1338136a3
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-05-26 16:50:40 -0300

    atualizações

--- resources/assets/frontend/js/components/FooterComponent.vue
+++ resources/assets/frontend/js/components/FooterComponent.vue
@@ -236,7 +236,7 @@
               	
               	var mutationObserver = new MutationObserver(function(mutations) {
 				  	mutations.forEach(function(mutation) {
-				    	console.log($(mutation.target).find('.skiptranslate'));
+				    	console.log($(mutation.target).find('div.skiptranslate'));
 				  	});
 				});
 
