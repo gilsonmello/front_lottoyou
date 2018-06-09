98c1d5694def3d1f5f6dc78e6b92a167a1820d70
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-06-09 13:37:54 -0300

    atualizações

--- app/Services/Frontend/Pagseguro/PaymentService.php
+++ app/Services/Frontend/Pagseguro/PaymentService.php
@@ -19,20 +19,6 @@ trait PaymentService
 {
     public function updateFromPagseguroFeedback($dataXml) 
     {
-        // Get cURL resource
-        $curl = curl_init();
-        // Set some options - we are passing in a useragent too here
-        curl_setopt_array($curl, array(
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => 'https://api.hgbrasil.com/finance?format=json&key=d58b8e61',
-        ));
-        // Send the request & save response to $resp
-        $resp = curl_exec($curl);
-        // Close request to clear up some resources
-        curl_close($curl);
-
-        $resp = json_decode($resp);
-
         //Log::info($resp->results->currencies->USD->buy);
 
         $pagseguroOrder = new PagseguroOrder;
