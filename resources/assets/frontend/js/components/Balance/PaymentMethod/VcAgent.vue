87ef7d0283fca28162791bee1abbf26d640231b5
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-06-12 14:53:54 -0300

    atualizações

--- resources/assets/frontend/js/components/Balance/PaymentMethod/VcAgent.vue
+++ resources/assets/frontend/js/components/Balance/PaymentMethod/VcAgent.vue
@@ -6,6 +6,51 @@
             </h6>
             <h5>
             </h5>
+
+            <table class="table table-hover table-striped table-responsive">
+                <tbody>
+                <tr style="border: none;">
+                    <td>{{ trans('strings.name') }}</td>
+                    <td>George Telles</td>
+                </tr>
+                <tr>
+                    <td>{{ trans('strings.coin') }}</td>
+                    <td>USD</td>
+                </tr>
+                <tr>
+                    <td>Retirada Mín/Máxima</td>
+                    <td>10 / 2000</td>
+                </tr>
+                <tr>
+                    <td>Resumo</td>
+                    <td>Transferências Bancárias</td>
+                </tr>
+                <tr>
+                    <td>Comissão sobre depósitos</td>
+                    <td>1,99%</td>
+                </tr>
+                <tr>
+                    <td>Comissão sobre retiradas</td>
+                    <td>1,99%</td>
+                </tr>
+                <tr>
+                    <td>{{ trans('strings.email') }}</td>
+                    <td>george_1@hotmail.com.br</td>
+                </tr>
+                <tr>
+                    <td>{{ trans('strings.phone') }}</td>
+                    <td>+55 71 988229457</td>
+                </tr>
+                <tr>
+                    <td>Outras informações</td>
+                    <td>
+                        Somos uma plataforma de pagamentos e transferência online,<br>
+                        trabalhamos com: Depósitos Bancários, transferências bancárias e Bitcoin
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+
             <h6><strong>O que é um Agente de Pagamento?</strong></h6>
             <h6 style="text-align: justify">
                 Um agente de pagamentos atua como um intermediário entre os clientes e a Lottoyou.bet. O agente de pagamentos processa depósitos e retiradas para os clientes que desejam usar métodos específicos de dinheiro eletrônico ou moedas locais não oferecidos pela Lottoyou.bet.
@@ -13,35 +58,36 @@
 
             <br>
             <h6>
-                <strong>Passos para realizar o pagamento:</strong>
+                Passos:
             </h6>
 
             <ol>
                 <li>
-                    Selecionar o país onde será realizado o pagamento;
+                    Entre em contato com o agente de pagamento disponível da sua região.
                 </li>
                 <li>
-                    Entrar em contato com o agente de pagamento disponível da sua região
+                    Escolha o método de pagamento que você prefere usar e envie os seus fundos ao agente de pagamento.
                 </li>
                 <li>
-                    Realizar o depósito na conta do agente de pagamentos.
+                    Encaminhar o comprovante de transferência para o agente de pagamentos.
                 </li>
                 <li>
-                    Encaminhar o comprovante de transferência para o e-mail: contato@lottoyou.bet
-                </li>
-                <li>
-                    Após confirmação o valor será creditado na conta Lottoyou.bet
+                    Após confirmação o valor liquido será creditado na conta Lottoyou.bet.
                 </li>
             </ol>
             <h6>
-                Este processo pode demorar até 3 horas após a transferência.
+                Este processo pode demorar até 24 horas após a confirmação do valor na conta do agente de pagamento.
             </h6>
             <h6>
                 <strong>
                     Importante salientar que as taxas de transferência são arcadas pelo cliente.
                 </strong>
             </h6>
-            <br>
+            <h6>
+                <strong>
+                    Por que os clientes escolhem usar agentes de pagamentos?
+                </strong>
+            </h6>
             <ul>
                 <li>
                     Aceitar transferências bancárias locais sem necessidade de ter cartão de crédito.
@@ -53,53 +99,10 @@
                     Permite que clientes depositem valores mais baixos (através da moeda local).
                 </li>
                 <li>
-                    É rápido e seguro.
+                    É rápido e seguro
                 </li>
             </ul>
             <br>
-            <table class="table table-hover table-striped table-responsive">
-                <tbody>
-                    <tr style="border: none;">
-                        <td>{{ trans('strings.name') }}</td>
-                        <td>George Telles</td>
-                    </tr>
-                    <tr>
-                        <td>{{ trans('strings.coin') }}</td>
-                        <td>USD</td>
-                    </tr>
-                    <tr>
-                        <td>Retirada Mín/Máxima</td>
-                        <td>10 / 2000</td>
-                    </tr>
-                    <tr>
-                        <td>Resumo</td>
-                        <td>Transferências Bancárias</td>
-                    </tr>
-                    <tr>
-                        <td>Comissão sobre depósitos</td>
-                        <td>1,99%</td>
-                    </tr>
-                    <tr>
-                        <td>Comissão sobre retiradas</td>
-                        <td>1,99%</td>
-                    </tr>
-                    <tr>
-                        <td>{{ trans('strings.email') }}</td>
-                        <td>george_1@hotmail.com.br</td>
-                    </tr>
-                    <tr>
-                        <td>{{ trans('strings.phone') }}</td>
-                        <td>+55 71 988229457</td>
-                    </tr>
-                    <tr>
-                        <td>Outras informações</td>
-                        <td>
-                            Somos uma plataforma de pagamentos e transferência online,<br>
-                            trabalhamos com: Depósitos Bancários, transferências bancárias e Bitcoin
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
         </div>
     </div>
 </template>
