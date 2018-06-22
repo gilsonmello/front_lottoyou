012c3e10aa9c86a8555d05b02cb32ccda6058e47
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-06-22 16:09:28 -0300

    atualizações

--- resources/assets/frontend/js/components/User/Transaction/VcOrderItem.vue
+++ resources/assets/frontend/js/components/User/Transaction/VcOrderItem.vue
@@ -5,13 +5,13 @@
 <template>
 	<div>
 		<span v-if="order_item.scratch_card">
-			{{ order_item.scratch_card.nome }}, Qtd. {{ typeof order_item.data == 'object' ? order_item.data.scratch_card.discount_tables.quantity : '' }}
+			{{ order_item.scratch_card.nome }}, Qtd. {{ order_item.quantity }}
 		</span>
 		<span v-else-if="order_item.soccer_expert">
-			{{ order_item.soccer_expert.nome }}, Qtd. {{ typeof order_item.data == 'object' ? order_item.data.tickets.length : '' }}
+			{{ order_item.soccer_expert.nome }}, Qtd. {{ order_item.quantity }}
 		</span>
 		<span v-else-if="order_item.lottery">
-			{{ order_item.lottery.nome }}, Qtd. {{ typeof order_item.data == 'object' ? order_item.data.tickets.length : '' }}
+			{{ order_item.lottery.nome }}, Qtd. {{ order_item.quantity }}
 		</span>
 		<router-link :to="{name: 'users.games', query: {id: order_item.id, page: 1, column: 'created_at', direction: 'asc'}}" class="btn btn-xs btn-primary">
 			<i class="fa fa-eye" data-toggle="tooltip" data-placement="top" :title="trans('strings.view')">
@@ -25,7 +25,7 @@
 	export default {
 		props: ['order_item'],
 		mounted() {
-			this.order_item.data = JSON.parse(this.order_item.data);
+			
 		}
 	}
 </script>
