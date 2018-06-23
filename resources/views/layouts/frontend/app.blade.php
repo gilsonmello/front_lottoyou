0319d630fbd708748457b62a8ecf9527b3d21b14
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-06-23 17:46:35 -0300

    atualizações

--- resources/views/layouts/frontend/app.blade.php
+++ resources/views/layouts/frontend/app.blade.php
@@ -101,6 +101,15 @@
         <script type="text/javascript" src="{{ asset('frontend/js/app.js') }}"></script>
         
        
+       <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121313526-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-121313526-1');
+        </script>
+
         
     </body>
 </html>
