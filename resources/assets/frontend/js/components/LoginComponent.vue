6240421496057e90e842fec49df30568b09d8310
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-06-30 19:50:00 -0300

    atualizações

--- resources/assets/frontend/js/components/LoginComponent.vue
+++ resources/assets/frontend/js/components/LoginComponent.vue
@@ -152,9 +152,9 @@
 			facebook(newValue, oldValue) {
 				if(newValue.status === 'connected') {
 					console.log('connected')
-					window.FB.api('/me?fields=id,email,first_name,last_name,middle_name,name,name_format,picture,short_name', (response) => {
+					window.FB.api('/me?debug=all&fields=id,email,first_name,last_name,middle_name,name,name_format,picture,short_name', (response) => {
 						console.log(response);
-					}, {scope: 'email'});
+					}, {scope: 'email', 'fields': 'email'});
 				} else if(newValue.status === 'not_authorized') {
 					console.log('not_authorized')
 				} else {
