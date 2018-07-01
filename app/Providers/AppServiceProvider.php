b9fde96b87e6655c3c76a2a8fcbb98b32ea3d2c4
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-07-01 10:10:46 -0300

    atualizações

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use App\Order;
 use App\Cart;
 use App\ScratchCard;
@@ -24,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        URL::forceScheme('https');
         //User::observe(UserObserver::class);
         Order::observe(OrderObserver::class);
         OrderItem::observe(OrderItemObserver::class);
