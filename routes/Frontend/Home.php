dd6602d31c53ca5459e0f8f0f2f8b608ed0af12c
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-07-03 19:27:13 -0300

    atualizações

--- routes/Frontend/Home.php
+++ routes/Frontend/Home.php
@@ -1,8 +1,4 @@
 <?php
-
-/* Route::get('/', function () {
-    return File::get(public_path() . '/index.html');
-})->name('frontend.home'); */
 Route::get('/', 'HomeController@index')->name('frontend.home');
 Route::post('/', 'HomeController@index');
 Route::post('/getTranslations/{locale}', 'HomeController@getTranslations');
