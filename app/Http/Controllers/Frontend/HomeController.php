a643f2366e9e3cab79bc4c1dc8d40a1b621a5d8f
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-07-03 19:27:35 -0300

    atualizações

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -9,33 +9,6 @@ use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
-    private function render($path) {
-        $renderer_source = File::get(base_path('node_modules/vue-server-renderer/basic.js'));
-        $app_source = File::get(public_path('js/entry-server.js'));
-    
-        $v8 = new \V8Js();
-    
-        ob_start();
-    
-        $js = 
-    <<<EOT
-    var process = { env: { VUE_ENV: "server", NODE_ENV: "production" } }; 
-    this.global = { process: process }; 
-    var url = "$path";
-    EOT;
-    
-        $v8->executeString($js);
-        $v8->executeString($renderer_source);
-        $v8->executeString($app_source);
-    
-        return ob_get_clean();
-      }
-    
-      public function get(Request $request) {
-        $ssr = $this->render($request->path());
-        return view('app', ['ssr' => $ssr]);
-      }
-      
     /**
      * Display a listing of the resource.
      *
