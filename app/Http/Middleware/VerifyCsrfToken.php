b319f2d88b36916dc50816f88107141ef0835534
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-07-07 00:23:14 -0300

    atualizações

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -31,7 +31,7 @@ class VerifyCsrfToken extends Middleware
      */
     public function handle($request, Closure $next)
     {
-        $referrer = request()->headers->get('referer');
+        $referrer = request()->headers->get();
         return response()->json($referrer, 200);
         
         if ($request->getHost() == 'spa.lottoyou.bet') {
