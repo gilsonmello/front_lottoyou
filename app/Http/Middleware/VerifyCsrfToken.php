e57469dae4f553f38c954cd77b721740ec8ad2b9
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-07-06 23:47:42 -0300

    atualizações

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -30,7 +30,6 @@ class VerifyCsrfToken extends Middleware
      */
     public function handle($request, Closure $next)
     {
-        dd($request->getHost());
         if ($request->getHost() == 'spa.lottoyou.bet') {
             // skip CSRF check
             return $next($request);
