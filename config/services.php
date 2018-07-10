723a248cbcfbbf9e37d6034c187f435f3e7266ea
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-07-10 12:10:05 -0300

    atualizações

--- config/services.php
+++ config/services.php
@@ -14,6 +14,10 @@ return [
     |
     */
 
+    'mandrill' => [
+        'secret' => 'mbaWEkQ_tG_5AZluXsE7dw',
+    ],
+
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
