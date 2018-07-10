ffa6f886cc6bcbb9aa8be5e53aa914b2cbcb750a
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-07-10 13:53:58 -0300

    atualizações

--- config/app.php
+++ config/app.php
@@ -151,7 +151,7 @@ return [
 
         Laravel\Passport\PassportServiceProvider::class,
 
-        Sichikawa\LaravelSendgridDriver\SendgridTransportServiceProvider::class
+        Sichikawa\LaravelSendgridDriver\SendgridTransportServiceProvider::class,
 
         /*
          * Package Service Providers...
