9112db0ea52e0d456434a2583c69ddff61a390e1
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-07-10 00:41:41 -0300

    atualizações

--- resources/assets/frontend/js/components/LoginComponent.vue
+++ resources/assets/frontend/js/components/LoginComponent.vue
@@ -64,18 +64,18 @@
 					</form>
                 </div>
                 <!-- Modal footer -->
-                <!-- <div class="modal-footer"> -->
+                <div class="modal-footer">
                 	<!-- <fb:login-button 
 					  scope="public_profile,email"
 					  onlogin="authLogin">
 					</fb:login-button> -->
-					<!-- <a class="btn btn-block btn-social btn-facebook" @click.prevent="loginFacebook">
+					<a class="btn btn-block btn-social btn-facebook" @click.prevent="loginFacebook">
 						<span class="fa fa-facebook"></span> Cadastre-se com o facebook
-					</a> -->
+					</a>
 					<!-- <button type="button" class="btn btn-info" @click="logoutFacebook" data-dismiss="modal">
 						Sair
                     </button> -->
-                <!-- </div> -->
+                </div>
             </div>
         </div>
     </div>
