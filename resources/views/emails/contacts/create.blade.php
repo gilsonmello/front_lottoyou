9f4bd20162006150a94592dfbc547d6f63e68545
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-07-10 22:56:47 -0300

    atualizações

--- resources/views/emails/contacts/create.blade.php
+++ resources/views/emails/contacts/create.blade.php
@@ -1,7 +1,7 @@
 <h2>RECEBEMOS A SUA CONSULTA</h2>
 <p>A equipe de suporte da Lottoyou.bet estará trabalhando para responder a sua consulta no prazo máximo de 72 horas.</p>
 <p>O número de protocolo é: {{ $contact->id }}</p>
-<p>Precisa de ajuda rápida? Encontre respostas conferindo no nosso site o <a href="{{route('frontend.home')}}/#/help">ajuda</a></p>
+<p>Precisa de ajuda rápida? Encontre respostas conferindo no nosso site o <a href="{{route('frontend.home')}}/help">ajuda</a></p>
 <br>
 <p>Atenciosamente,</p>
 <p>Equipe Lottoyou<br>Aqui é mais fácil</p>
