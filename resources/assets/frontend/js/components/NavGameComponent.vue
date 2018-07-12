b93ca8dc3912b6e575d14473f198396097a3d1d9
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-07-12 17:11:07 -0300

    atualizações

--- resources/assets/frontend/js/components/NavGameComponent.vue
+++ resources/assets/frontend/js/components/NavGameComponent.vue
@@ -34,7 +34,7 @@
 
 				<router-link class="world_cup_now" tag="li" :to="{ name: 'soccer_expert.show', params: {id: 2} }">
 					<a class="nav-link">
-						{{ trans('strings.world_cup_now') }}
+						{{ trans('strings.libertadores_now') }}
 					</a>
 				</router-link>
 	            <!-- <li class="nav-item">
