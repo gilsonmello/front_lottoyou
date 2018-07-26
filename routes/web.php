31112e82d2375fa5b18f9d86b3c8a5451aa2ce6c
Gilson Melo <junnyorr.sirnandes@gmail.com>
2018-07-26 11:45:14 -0300

    atualizações

--- routes/web.php
+++ routes/web.php
@@ -73,7 +73,7 @@ Route::get('/', function () {
 	return view('layouts.frontend.app');
 })->name('frontend.home');
 
-$_SERVER['HTTP_USER_AGENT'] = 'googlebot';
+//$_SERVER['HTTP_USER_AGENT'] = 'googlebot';
 
 if(isset($_SERVER['HTTP_USER_AGENT']) && 
 	strstr(strtolower($_SERVER['HTTP_USER_AGENT']), "googlebot"))
