514dbeb87339b133c4a487cf9cdbfac4bcceb111
Gilson Oliveira <junnyorr.sirnandes@gmail.com>
2019-03-27 11:21:56 -0300

    atualizações

--- routes/web.php
+++ routes/web.php
@@ -329,7 +329,7 @@ Route::get('/{param_1}/{param_2?}', function ($param_1 = null, $param_2 = null, 
 })
 ->where('param_2', '(.*)');
 
-Route::get('/'.config('app.url_locale').'users/password_recovery/{hash}', 'UserController@passwordRecovery')
+Route::get('users/password_recovery/{hash}', 'UserController@passwordRecovery')
 	->name('frontend.users.password_recovery');
 	
 Route::get('users/activate/{hash}', 'UserController@activate')
